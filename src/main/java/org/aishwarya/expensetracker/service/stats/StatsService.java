4765e1791ec6268eb707f0163d549e5779df01b2
Aishwarya Lokhande <aisha29301@gmail.com>
2024-06-02 23:36:59 +0530

    first commit

--- src/main/java/org/aishwarya/expensetracker/service/stats/StatsService.java
+++
@@ -1,9 +0,0 @@
-package org.aishwarya.expensetracker.service.stats;
-
-import org.aishwarya.expensetracker.dto.GraphDTO;
-import org.aishwarya.expensetracker.dto.StatsDTO;
-
-public interface StatsService {
-    GraphDTO getChartData();
-    StatsDTO getStats();
-}
